4a2f54229c95f25bcf276ca91dc5f4c3d47120e4
spykelionel <social.lionel@gmail.com>
2024-03-03 17:40:22 +0100

    some minute touches

--- index.php
+++ index.php
@@ -1,16 +1,28 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
 <head>
-<title>Today&rsquo;s Date</title>
-<meta http-equiv="content-type"
-content="text/html; charset=utf-8"/>
+    <title>Today&rsquo;s Date</title>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
 </head>
+
 <body>
-<p>Today&rsquo;s date (according to this web server) is
-<?php
-echo 'This is a <strong>test</strong>!';
-?>
-</p>
+    <form action="" method="post">
+        <div><label for="name">Name:
+                <input type="text" name="name" id="name" /></label></div>
+        <div><input type="submit" value="GO" /></div>
+    </form>
+
+    <?php
+    if ($_REQUEST['name'] != null) {
+        $name = $_REQUEST['name'];
+    }
+    if ($name == 'Kevin') {
+        echo 'Welcome, oh glorious leader! ';
+    } else {
+        echo "Welcome {$name}";
+    }
+    ?>
 </body>
+
 </html>
